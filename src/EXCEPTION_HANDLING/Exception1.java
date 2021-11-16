ae0425102f1f4f7e757b7dda3c0c2a1979a4cb2f
HARSH CHOUDHARY <harsh.singh_cs20@gla.ac.in>
2021-11-16 08:23:07 +0530

    adding question

--- src/EXCEPTION_HANDLING/Exception1.java
+++ src/EXCEPTION_HANDLING/Exception1.java
@@ -1,4 +1,40 @@
 package EXCEPTION_HANDLING;
+/*
+Write a program that takes as input the size of the array and the elements in the array. The program then asks the user to enter a particular index and prints the element at that index. Index  starts from zero.
+
+This program may generate Array Index Out Of Bounds Exception  or NumberFormatException .  Use exception handling mechanisms to handle this exception.
+
+Sample Input and Output 1:
+Enter the number of elements in the array
+2
+Enter the elements in the array
+50
+80
+Enter the index of the array element you want to access
+1
+The array element at index 1 = 80
+The array element successfully accessed
+
+
+ Sample Input and Output 2:
+Enter the number of elements in the array
+2
+Enter the elements in the array
+50
+80
+Enter the index of the array element you want to access
+9
+java.lang.ArrayIndexOutOfBoundsException
+
+
+ Sample Input and Output 3:
+Enter the number of elements in the array
+2
+Enter the elements in the array
+30
+j
+java.lang.NumberFormatException
+ */
 
 import java.util.Scanner;
 
